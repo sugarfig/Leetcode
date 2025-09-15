4efcb3b9c7de9d25b716c4947197e705b9a4123f
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-15 23:11:18 +0100

    Time: 127 ms (5.42%) | Memory: 14.9 MB (6.18%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,59 @@
+class Solution {
+// take letter of the first one and as soon as you see a letter that doesnt match the first then return nothing
+// if first letter matches first letter of everything else then go back to the first one and add second letter
+// make sure to keep running tab of letter matches & current letters being tested. 
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string letterMatches = "";
+        string currentTestMatch = "";
+        bool allMatched = false;
+
+        if (strs.size() == 0)
+        {
+            return "";
+        }
+
+        if (strs.size() == 1)
+        {
+            return strs[0];
+        }
+
+        // currentTestMatch.push_back(strs[0][0]);
+
+        for(int j = 0; j < strs[0].size(); j++)
+        {
+            currentTestMatch.push_back(strs[0][j]);
+            std::cout << "currentTestMatch:" << currentTestMatch << endl;
+
+            for (int i = 1; i < strs.size(); i++)
+            {
+                if ((currentTestMatch.length() <= strs[i].length()) && (currentTestMatch == strs[i].substr(0, currentTestMatch.length())))
+                {
+                    allMatched = true;
+                    std::cout << currentTestMatch << " matches at " << i << endl;
+                }
+                else {
+                    allMatched = false;
+                    break;
+                }
+                // for (int j = 0; j < strs[i].length(); j++)
+                // {
+                //     currentTestMatch.push_back(strs[i][j]);
+                //     // currentTestMatch = strs[i][j];
+                //     std::cout << "currentTestMatch:" << currentTestMatch << endl;
+                // }
+                // std:cout << "here";
+            }
+
+            if(!allMatched)
+            {
+                return letterMatches;
+            }
+
+            letterMatches = currentTestMatch;
+        }
+
+        return letterMatches;
+        
+    }
+};
