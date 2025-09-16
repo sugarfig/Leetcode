f466628b369f57ea84b2d70d597fac8b862236c0
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-16 06:17:12 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (12.23%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,4 +1,13 @@
 class Solution:
+    # checks if there is an odd number of values in the string
+    # if yes, then just return false
+    # if no, continue
+    # loop thru string and
+    # push opening brackets, braces, and parenthesis into a stack
+    # then pop once it sees a closing brace, bracket, or parenthesis and if not corresponding opening brace is popped then return false.
+    # need to check that there is something in the stack to pop, and if not, then it is invalid bc there was no opening one so return false as well.
+    # once done with loop, check if any remaining values are in the stack. if yes, then return false. if no, continue
+    # returns true if never returned false
     def isValid(self, s: str) -> bool:
         # print(len(s) % 2 != 0)
 
