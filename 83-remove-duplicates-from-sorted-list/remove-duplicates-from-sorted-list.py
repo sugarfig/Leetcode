c9c331def2a0ce9791fb802b4cda9f2b1ebf1be6
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-17 11:03:05 +0100

    Time: 71 ms (2.63%) | Memory: 18.1 MB (21.74%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -7,11 +7,12 @@ class Solution:
     # have 2 pointers one that points at head at first and another that points to next
     # need to check if empty and if only one node. if either of these, then just return head
     # if not, then create nextCurr
-    # loop until curr or nextCurr become none
+    # loop until nextCurr become none
     # if curr.val is equal to nextCurr.val then have current next point to nextCurr next (not to nextCurr because those 2 are the same)
     # if not equal then just move curr to the next one
     # always move nextCurr by one
-    # keep looping until 
+    # keep looping until nextCurr becomes none
+    # return head bc this is what we rearranged/deleted from
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
         if curr == None or curr.next == None:
