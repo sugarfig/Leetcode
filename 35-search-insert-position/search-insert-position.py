3db0d520b90a94689557efcc804e1e178b57d980
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-18 02:19:40 +0100

    Time: 0 ms (100.00%) | Memory: 18.4 MB (73.35%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        min = 0
+        max = len(nums)-1
+        mid = math.floor((min + max) /2)
+        while min <= max:
+            val = nums[mid]
+            if target == val:
+                return mid
+            if target < val:
+                max = mid-1
+            else:
+                min = mid+1
+            mid = math.floor((min + max) /2)
+        return min
+            
