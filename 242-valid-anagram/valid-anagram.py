27e14af6d7cd18169b493d1308d937de7b6aa87d
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-19 16:11:08 -0700

    Time: 11 ms (72.33%) | Memory: 17.7 MB (97.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,29 @@
+class Solution:
+    # first check if lengths match
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        mapString1 = {}
+        mapString2 = {}
+
+        # for char1 in s:
+        #     if char in mapString1:
+        #         mapString1[char] += 1
+        #     else:
+        #         mapString1[char] = 1
+        
+        # for char in t:
+        #     if char in mapString2:
+        #         mapString2[char] += 1
+        #     else:
+        #         mapString2[char] = 1
+
+        for char1, char2 in zip(s, t):
+            mapString1[char1] = mapString1.get(char1, 0) + 1
+            mapString2[char2] = mapString2.get(char2, 0) + 1
+
+        if len(mapString1) != len(mapString2):
+            return False
+        
+        return mapString1 == mapString2
