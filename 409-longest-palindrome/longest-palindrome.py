fffd47bdb66c7babc43114659832de9066bce046
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-23 16:38:35 -0700

    Time: 3 ms (40.21%) | Memory: 17.8 MB (75.87%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,5 +1,12 @@
 class Solution:
+    # if length is 1, that is the longest palindrome
     # create a hashmap and count how many times a specific character appears in the string
+    # loop thru the hashmap and check if the value in the hashmap is 2 or over
+    # if it is, then add how many pairs it has to a counter called pairs
+    # if not, don't do anything
+    # result will be the amount of pairs times 2
+    # if the length of the string is less than the amount in result then add 1
+    # return result
     def longestPalindrome(self, s: str) -> int:
         pairs = 0
         charMap = {}
@@ -9,11 +16,15 @@ class Solution:
             charMap[char] = charMap.get(char, 0) + 1
         
         for key, value in charMap.items():
-            if value >= 2:
-               pairs = pairs + math.floor(value / 2)
+            # if value >= 2:
+            #    pairs = pairs + math.floor(value / 2)
+            if value % 2 == 0:
+                pairs = pairs + value
+            else:
+                pairs = pairs + (value - 1)
             
-        result = pairs * 2
-        if result < len(s):
-            return int(result + 1)
+        # result = pairs * 2
+        if pairs < len(s):
+            return int(pairs + 1)
 
-        return int(result)
+        return int(pairs)
