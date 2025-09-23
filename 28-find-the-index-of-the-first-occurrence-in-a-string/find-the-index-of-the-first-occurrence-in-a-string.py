bc0a4a398a88b2d6c15043407d5d77f3107753b5
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-23 13:13:52 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (10.45%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,3 +1,5 @@
 class Solution:
+    # use build in function (.find)
+    # this function returns the index of the first letter of the first instance of that string and if not found, returns -1
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
