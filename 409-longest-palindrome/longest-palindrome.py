a34bb4423ff990d1e54b6589f86554ca7d93f6b4
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-23 16:27:17 -0700

    Time: 1 ms (56.77%) | Memory: 17.8 MB (45.79%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,7 +1,10 @@
 class Solution:
+    # create a hashmap and count how many times a specific character appears in the string
     def longestPalindrome(self, s: str) -> int:
         pairs = 0
         charMap = {}
+        if len(s) == 1:
+            return 1
         for char in s:
             charMap[char] = charMap.get(char, 0) + 1
         
