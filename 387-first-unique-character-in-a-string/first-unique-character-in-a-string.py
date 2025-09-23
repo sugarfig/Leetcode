f8cd15c9d52ea5676d9283c9b6982474d48c94ed
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-23 12:46:31 -0700

    Time: 75 ms (27.42%) | Memory: 18.1 MB (36.36%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,4 +1,8 @@
 class Solution:
+    # count all the chars in the string s and add this to the hashmap and have the char be the key and the amount of times it shows up the value
+    # loop thru the string s again and find each character in the hashmap and if the value for that character is greater than 1, then keep going and if not it is equal to 1 then return that index
+    # if the for loop finishes without returning any index, then return -1
+    # September 23, 2025 #16
     def firstUniqChar(self, s: str) -> int:
         charMap = {}
         for char in s:
