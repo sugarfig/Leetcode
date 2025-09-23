f565f17f0be0a0eb0a3c5a34d2b2fef4b827a805
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-23 15:42:05 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (12.91%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,4 +1,10 @@
 class Solution:
+    # check to see which string was longer/shorter and add leading zeroes to the shorter one
+    # loop thru both lists (they should be the same length now)
+    # do a xor operation on both chars from string A and string B and an xor on carry then add to front of result string
+    # check if there should be a carry value and if not set back to zero, if yes then make carry 1
+    # check if there is still a carry value after for loop. if there is then add to the front of result.
+    # September 23, 2025 #18
     def addBinary(self, a: str, b: str) -> str:
         result = ""
         carry = 0
@@ -13,7 +19,7 @@ class Solution:
         
         for charA, charB in reversed(list(zip(a, b))):
             result = str(int(charA) ^ int(charB) ^ carry) + result
-            print(charA, charB)
+            # print(charA, charB)
             # print(charB)
             if charA == '1' and charB == '1' or charA == '1' and carry == 1 or charB == '1' and carry == 1:
                 carry = 1
