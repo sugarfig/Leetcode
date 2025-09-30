63da70cb9f469e216fc4845290bd35f7751c06f7
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-09-29 18:44:23 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.09%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,8 +1,13 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        # go thru nums and start at 1 to end of nums. add nums at index i - 1 to nums at index i then replace nums at index i
+        # ORRR append nums at index 0 to result
+        # go thru nums and start at 1 to end of nums. 
+        # add value at index i - 1 in result to nums at index i then append to result
+        
         if len(nums) < 0:
             return []
-
+        # this works too, but rewrote without needing the results array
         # result = []
         # result.append(nums[0])
         # for i in range(1, len(nums)):
