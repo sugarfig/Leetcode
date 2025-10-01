0f959303b8a651121825edd0a4def19ffbd4abba
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-10-01 04:51:51 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.49%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -4,23 +4,39 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # first way i did:
+    # walk thru linked list and get amount in linked list
+    # then divide by 2 to get middle
+    # start again from top and go to middle
+    # if amount in linked list was even then go to next bc this algorithm will take the first middle one.
+    # 2nd way to do:
+    # have 2 pointers start at 1st linked list head then increase one by 2 and then increase the other by 1 everytime
+    # do this while walker and walker.next are not null
+
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        walker = head
-        counter = 0
-        if head == None:
-            return None
+        # walker = head
+        # counter = 0
+        # if head == None:
+        #     return None
         
-        while walker != None:
-            counter += 1
-            walker = walker.next
-        # print(counter)
-        middle = math.ceil(counter / 2)
-        # print(middle)
-        newWalker = head
-        for i in range(1, middle):
-            newWalker = newWalker.next
+        # while walker != None:
+        #     counter += 1
+        #     walker = walker.next
+        # # print(counter)
+        # middle = math.ceil(counter / 2)
+        # # print(middle)
+        # newWalker = head
+        # for i in range(1, middle):
+        #     newWalker = newWalker.next
 
-        if counter % 2 == 0:
-            newWalker = newWalker.next
+        # if counter % 2 == 0:
+        #     newWalker = newWalker.next
 
-        return newWalker
+        # return newWalker
+
+        walker = head
+        middle = head
+        while walker != None and walker.next != None:
+            middle = middle.next
+            walker = walker.next.next
+        return middle
