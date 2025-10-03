3d7e9ab96055c682d0aee26c9e5705bd71420372
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-10-02 17:41:54 -0700

    Time: 3 ms (61.45%) | Memory: 18 MB (80.96%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.py
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -1,4 +1,12 @@
 class Solution:
+    # go thru twice
+    # first time get the amount of zeros in it before we reach the max amount of spaces in the array
+    # then that amount will basically be the amount that we "chop" off the end
+    # if last is a 0 before got chopped then just put that at the end of the array
+    # start from end of array
+    # if index value is 0 then put a 0 at current count + index value
+    # subtract one from count then put another zero at count + index
+    # otherwise just put value at current index at index + count
     def duplicateZeros(self, arr: List[int]) -> None:
         """
         Do not return anything, modify arr in-place instead.
