bc7aeeb9f976ad33df701463ad30a814953ccb4e
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-10-07 14:07:34 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (48.34%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,4 +1,10 @@
 class Solution:
+    # have 2 pointers. one will be a reader. the other will be a writer. reader will start at index 1 and writer will start at index 0
+    # we have a while loop so while reader is not at the end of the array
+    # if the value at reader and writer are not equal, then increment writer and then put val at reader to writer
+    # add 1 to reader every loop
+    # after loop is done, get the amount of indicies we need to pop from nums by doing lens of nums minus writer which is at the largest index that does not reapeat any vals
+    # then loop thru that number and pop that many times
     def removeDuplicates(self, nums: List[int]) -> int:
         reader = 1
         writer = 0
