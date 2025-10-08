f620f431baa0bacd31e90f0bbae29c47f05822b9
sugarfig <81268104+sugarfig@users.noreply.github.com>
2025-10-08 01:24:43 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.42%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,4 +1,7 @@
 class Solution:
+    # go thru array and for every number add 1 to the value in the hashmap
+    # now loop thru hashmap and check if the key is zero and if it is, then check if the value is more than 1. if so, return true
+    # check if value is 
     def checkIfExist(self, arr: List[int]) -> bool:
         dictionary = {}
         
@@ -7,13 +10,12 @@ class Solution:
             # print(arr[i])
         
         for k, v in dictionary.items():
+            # written with multiple if statments for readability
             if k == 0 and v > 1:
                 return True
-            if k != 0 and k / 2 in dictionary:
-                print("k/2", k/2, v)
-                return True
+            # if k !=0 and k / 2 in dictionary:
+            #     return True
             if k != 0 and k * 2 in dictionary:
-                # print("k * 2")
                 return True
             
         return False
