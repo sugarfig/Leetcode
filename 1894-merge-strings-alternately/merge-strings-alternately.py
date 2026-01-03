36ead361ad68274e76b705e75e83839947d37c61
sugarfig <81268104+sugarfig@users.noreply.github.com>
2026-01-02 16:35:08 -0800

    Time: 31 ms (90.02%) | Memory: 17.4 MB (79.52%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,15 +1,34 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = []
-        i = j = 0
+        # loop for shortest word times 2 cause we are gonna alternate with the other word.
+        # alternate and keep bool to track which word we are on.
+        # once done with shortest word. append the rest of the other word to result string.
 
-        while i < len(word1) or j < len(word2):
-            if i < len(word1):
-                res.append(word1[i])
-                i += 1
-            if j < len(word2):
-                res.append(word2[j])
-                j += 1
+        shortestWord = ""
+        word1Go = True
+        result = ""
+        word1Counter = 0
+        word2Counter = 0
 
-        return "".join(res)
+        if len(word1) < len(word2):
+            shortestWord = word1
+        else:
+            shortestWord = word2
+        
+        for count in range(len(shortestWord)*2):
+            if word1Go:
+                result = result + word1[word1Counter]
+                word1Counter+= 1
+                word1Go = False
+            else:
+                result = result + word2[word2Counter]
+                word2Counter+=1
+                word1Go = True
+
+        if word1 == shortestWord:
+            result = result + word2[word2Counter:]
+        else:
+            result = result + word1[word1Counter:]
+        
+        return result
         
