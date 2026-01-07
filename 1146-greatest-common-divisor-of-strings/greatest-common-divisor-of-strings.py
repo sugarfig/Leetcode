444e592d84c0aa7111ae422e027c477bc5f80acd
sugarfig <81268104+sugarfig@users.noreply.github.com>
2026-01-06 16:26:43 -0800

    Time: 4 ms (16.53%) | Memory: 19.6 MB (5.83%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,7 +1,12 @@
 class Solution:
+    # take the shortest string and check if it is divisble by the base
+    # if it is divisible, multiply the base by the number of length gotten from division 
+    # for both string 1 and 2 and then check if new strings are equal to original strings
+    # if yes, return base
+    # if no, then remove last character from base and do again until base == ""
+    # if it is not divisble, remove last chacter from base and loop process/checks again until 
+    # base == ""
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # i = j = 0
-        # divisor = ""
         base = ""
         if len(str1) < len(str2):
             base = str1
@@ -9,34 +14,26 @@ class Solution:
             base = str2
         
         while base != "":
-            print("here")
+            # print("here")
             if (len(str1) % len(base)) == 0 and (len(str2) % len(base)) == 0:
-                print("here1")
-                str1Multiple = len(str1) // len(base)
-                str2Multiple = len(str2) // len(base)
+                # print("here1")
+                str1Multiple = len(str1) // len(base) # // does floor in division
+                str2Multiple = len(str2) // len(base) # // does floor in division
                 str1Test = base * str1Multiple
                 str2Test = base * str2Multiple
-                print("str1Multiple", str1Multiple)
-                print("str2Multiple", str2Multiple)
-                print("str1Test", str1Test)
-                print("str2Test", str2Test)
-                print("str1", str1)
-                print("str2", str2)
+                # print("str1Multiple", str1Multiple)
+                # print("str2Multiple", str2Multiple)
+                # print("str1Test", str1Test)
+                # print("str2Test", str2Test)
+                # print("str1", str1)
+                # print("str2", str2)
                 if str1Test == str1 and str2Test == str2:
                     return base
-                print("here2")
+                # print("here2")
                 base = base[:-1]
             else:
-                print("here3")
+                # print("here3")
                 base = base[:-1]
         return ""
-
-        # while i < len(str1) and j < len(str2):
-        #     if str1[i] != str2[j]:
-        #         return ""
-        #     else:
-        #         divisor = divisor + str1[i]
-        #         i += 1
-        #         j += 1
         
         
